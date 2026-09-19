42ac6931161edf95e3ca9c9dd7e10cb80d4fd659
Stefanos Balaskas <stefbalaskas@gmail.com>
2026-09-19 09:15:30 +0300

    Extend public API contract for FPCA subspace stability

--- tests/test_public_api.py
+++ tests/test_public_api.py
@@ -1,7 +1,7 @@
 import eyetrajectoriespy as et
 
 def test_version_and_public_symbols():
-    assert et.__version__=="0.4.0.dev0"
+    assert et.__version__=="0.5.0.dev0"
     required={
         "TrajectorySet","fit_fpca","fit_mfpca","fit_multilevel_fpca","fit_compositional_fpca",
         "register_to_landmarks","fit_elastic_fpca","simulate_planar_trajectories",
@@ -16,6 +16,11 @@ def test_version_and_public_symbols():
         "select_fpca_components_cv","bootstrap_fpca_component_envelopes",
         "plot_fpca_cross_validation","plot_fpca_component_envelope",
         "fpca_cross_validation_reporting_text","fpca_component_envelope_reporting_text",
+        "FPCASubspaceComparisonResult","FPCASubspaceStabilityResult",
+        "fpca_eigenvalue_gap_table","compare_fpca_subspaces",
+        "bootstrap_fpca_subspace_stability","summarise_fpca_subspace_stability",
+        "plot_fpca_subspace_stability","fpca_eigengap_reporting_text",
+        "fpca_subspace_stability_reporting_text",
     }
     assert required <= set(et.__all__)
     for name in required:
