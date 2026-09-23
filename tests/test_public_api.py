4e33f1af0fa0de8517657760d3aded74d2f7d55e
Stefanos Balaskas <stefbalaskas@gmail.com>
2026-09-24 00:41:11 +0300

    Update public API contract for 0.32

--- tests/test_public_api.py
+++ tests/test_public_api.py
@@ -1,7 +1,7 @@
 import eyetrajectoriespy as et
 
 def test_version_and_public_symbols():
-    assert et.__version__=="0.31.0.dev0"
+    assert et.__version__=="0.32.0.dev0"
     required={
         "MathematicalContract","get_mathematical_contract","list_mathematical_contracts","mathematical_contract_frame",
         "DelayEmbeddingResult","EmbeddingDelayDiagnosticResult","EmbeddingDimensionDiagnosticResult","RecurrenceResult","RecurrenceRadiusProfileResult","RQAResult","RQAMeanBootstrapResult","RQAParameterSensitivityResult","WindowedRQAResult","WindowedRQAFunctionalResult","WindowedRQASensitivityResult","WindowedRQAMeanBandResult",
@@ -16,7 +16,7 @@ def test_version_and_public_symbols():
         "surrogate_nonlinearity_reporting_text","return_map_stability_reporting_text",
         "TrajectorySet","fit_fpca","fit_mfpca","fit_multilevel_fpca","fit_compositional_fpca","FPCAScoreUncertaintyResult","FPCASpectrumUncertaintyResult",
         "register_to_landmarks","fit_elastic_fpca","simulate_planar_trajectories","heading_function","signed_curvature_function","turning_rate_function","trajectory_tortuosity",
-        "fit_scalar_on_function_regression","plot_fpca_component",
+        "discrete_frechet_distance","pairwise_discrete_frechet_distances","fit_scalar_on_function_regression","plot_fpca_component",
         "IrregularTrajectorySet","from_irregular_long_dataframe_native","bootstrap_fpca_stability",
         "fit_phase_fpca","compare_registered_unregistered_fpca","to_skfda_basis",
         "split_conformal_fpca_anomaly","conformal_fpca_anomaly_frame","plot_conformal_fpca_anomaly","conformal_fpca_anomaly_reporting_text","diagnose_fpca_outliers","leave_one_group_out_fpca_influence",
