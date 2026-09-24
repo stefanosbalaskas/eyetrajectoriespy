36a0c65b4d5eaa441838692a1f6547c99c4b7353
Stefanos Balaskas <stefbalaskas@gmail.com>
2026-09-24 12:55:11 +0300

    Advance public API version assertion to 0.39

--- tests/test_public_api.py
+++ tests/test_public_api.py
@@ -1,7 +1,7 @@
 import eyetrajectoriespy as et
 
 def test_version_and_public_symbols():
-    assert et.__version__=="0.38.0.dev0"
+    assert et.__version__=="0.39.0.dev0"
     required={
         "MathematicalContract","get_mathematical_contract","list_mathematical_contracts","mathematical_contract_frame",
         "DelayEmbeddingResult","EmbeddingDelayDiagnosticResult","EmbeddingDimensionDiagnosticResult","RecurrenceResult","RecurrenceRadiusProfileResult","RQAResult","RQAMeanBootstrapResult","RQAParameterSensitivityResult","WindowedRQAResult","WindowedRQAFunctionalResult","WindowedRQASensitivityResult","WindowedRQAMeanBandResult",
