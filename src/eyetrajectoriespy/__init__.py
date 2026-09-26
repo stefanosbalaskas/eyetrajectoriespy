51cad564d35d3368f852327fd26cabb3323ff813
Stefanos Balaskas <stefbalaskas@gmail.com>
2026-09-26 14:19:29 +0300

    Export portable result and environment APIs

--- src/eyetrajectoriespy/__init__.py
+++ src/eyetrajectoriespy/__init__.py
@@ -483,6 +483,12 @@ from .wild_regression import (
     fpca_wild_bootstrap_projection_frame,
     wild_bootstrap_fpca_projection,
 )
+from .portable_results import (
+    PortableScientificResultSnapshot,
+    capture_environment,
+    export_portable_result,
+    load_portable_result,
+)
 from .validation import (
     validate_common_grid,
     validate_no_long_missing_runs,
@@ -493,6 +499,10 @@ from .validation import (
 __version__ = "0.56.0.dev0"
 
 __all__ = [
+    "PortableScientificResultSnapshot",
+    "capture_environment",
+    "export_portable_result",
+    "load_portable_result",
     "GeneralizedFunctionOnScalarPredictionResult",
     "GeneralizedFunctionOnScalarPredictionBootstrapResult",
     "GeneralizedFunctionOnScalarPredictionBandResult",
